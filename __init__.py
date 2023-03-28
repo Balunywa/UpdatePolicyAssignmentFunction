6b5302a9b943047ceb5c135b72302d53113814ca
Balunywa <Lukman@taacs.cloud>
2023-03-27 20:17:04 -0400

    updates

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
-import logging
 import azure.functions as func
+import logging
+import requests
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import ResourceManagementClient
 from azure.mgmt.policyinsights.models import QueryOptions
@@ -8,24 +9,18 @@ from azure.mgmt.resource.policy import PolicyClient
 from azure.mgmt.policyinsights import PolicyInsightsClient
 from azure.mgmt.policyinsights.models import PolicyStatesResource
 from azure.identity import ClientSecretCredential
-import requests
-import json
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
 
-    # Check for webhook validation
-    validation_code = req.headers.get("ValidationCode")
-    if validation_code:
-        return func.HttpResponse(validation_code)
+def main(event: func.EventGridEvent):
+    logging.info('Python EventGrid trigger function processed an event: %s', event.get_json())
 
     # Set the target application name
-    app_name = "Azure Spring Cloud Resource Management"
+    target_app_name = "Azure Spring Cloud Resource Management"
 
     # Set your Azure AD credentials
-    tenant_id = "46ebbfcd-0b34-421b-95b6-0d3be04f5baf"
-    client_id = "6f8d280c-dbf1-464d-b79f-9918efbba95d"
-    client_secret = "Ksj8Q~n_RTrqsAsdFyWcp2Q_tXEMIzpi5rBD6bXN"
+    tenant_id = "fef3ddf9-5e8d-4606-907f-c575d0732655"
+    client_id = "b01e8aaa-1c3f-4cb4-b865-99ab718370b3"
+    client_secret = "Gfc8Q~kWWHUtwy3wZP2zbMFR2.u915H_RoRM.bG6"
 
     # Authenticate using the ClientSecretCredential
     credential = ClientSecretCredential(tenant_id, client_id, client_secret)
@@ -42,9 +37,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # Define the Graph API endpoint for listing service principals
     service_principals_url = "https://graph.microsoft.com/v1.0/servicePrincipals"
 
-    subscription_id = "baba5760-b841-47cc-ad3f-0097b63b2ac7"
+    subscription_id = "c801a0b0-c54c-4193-9caa-4d56a72099ad"
 
+    resource_client = ResourceManagementClient(credential, subscription_id)
     policy_client = PolicyClient(credential, subscription_id)
+    policyInsightsClient = PolicyInsightsClient(credential, subscription_id)
 
     def get_enterprise_app_object_id(app_name):
         try:
@@ -58,71 +55,58 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     if sp["displayName"].lower() == app_name.lower():
                         return sp["id"]
 
-                print(f"Could not find the enterprise application with the name '{app_name}'.")
+                logging.info(f"Could not find the enterprise application with the name '{app_name}'.")
                 return None
 
             else:
-                print(f"Error retrieving service principals: {response.status_code} - {response.text}")
+                logging.error(f"Error retrieving service principals: {response.status_code} - {response.text}")
                 return None
 
         except requests.exceptions.RequestException as e:
-            print(f"An error occurred while fetching the enterprise application object ID: {e}")
+            logging.error(f"An error occurred while fetching the enterprise application object ID: {e}")
             return None
 
-    object_id = get_enterprise_app_object_id(app_name)
-    logging.info(f"The object ID for '{app_name}' is: {object_id}")
-    
+    object_id = get_enterprise_app_object_id(target_app_name)
+    logging.info(f"The object ID for '{target_app_name}' is: {object_id}")
+
+
     # Set the assignment ID
-    assignment_id = "/subscriptions/baba5760-b841-47cc-ad3f-0097b63b2ac7/providers/Microsoft.Authorization/policyAssignments/2e18b578df1243699246e272"
+    assignment_id = "/subscriptions/c801a0b0-c54c-4193-9caa-4d56a72099ad/providers/Microsoft.Authorization/policyAssignments/428fc04ec56945b29e24ad07"
 
     # Get the policy assignment
     policy_assignment = policy_client.policy_assignments.get_by_id(assignment_id)
 
-    updated_policy_parameters = None
-    updated_policy_assignment = None
-
     if policy_assignment:
-    # Get the existing allowedPrincipalIDs parameter
-       existing_allowed_principals = policy_assignment.parameters["allowedPrincipalIDs"].value if "allowedPrincipalIDs" in policy_assignment.parameters else []
+        # Get the existing allowedPrincipalIDs parameter
+        existing_allowed_principals = policy_assignment.parameters["allowedPrincipalIDs"].value if "allowedPrincipalIDs" in policy_assignment.parameters else []
 
-    # Check if the object ID is already in the allowedPrincipalIDs list
-    if object_id in existing_allowed_principals:
-        logging.info("The object ID is already in the allowedPrincipalIDs list.")
-    else:
-        # Add the object ID to the allowedPrincipalIDs list
-        updated_allowed_principals = existing_allowed_principals + [object_id]
+        # Check if the object ID is already in the allowedPrincipalIDs list
+        if object_id in existing_allowed_principals:
+            logging.info("The object ID is already in the allowedPrincipalIDs list.")
+        else:
+            # Add the object ID to the allowedPrincipalIDs list
+            updated_allowed_principals = existing_allowed_principals + [object_id]
 
-        # Define the updated policy parameters
-        updated_policy_parameters = {
-            "allowedPrincipalIDs": {
-                "value": updated_allowed_principals
-            }
-        }
-
-    # Update the policy assignment with the new parameters, if needed
-    if updated_policy_parameters:
-        updated_policy_assignment = policy_client.policy_assignments.create(
-            policy_assignment.scope,
-            policy_assignment.name,
-            {
-                "location": policy_assignment.location,
-                "properties": {
-                    "policyDefinitionId": policy_assignment.policy_definition_id,
-                    "parameters": updated_policy_parameters
+            # Define the updated policy parameters
+            updated_policy_parameters = {
+                "allowedPrincipalIDs": {
+                    "value": updated_allowed_principals
                 }
             }
-        )
 
-        logging.info("The policy assignment has been updated with the new allowedPrincipalIDs.")
+            # Update the policy assignment with the new parameters
+            updated_policy_assignment = policy_client.policy_assignments.create(
+                policy_assignment.scope,
+                policy_assignment.name,
+                {
+                    "location": policy_assignment.location,
+                    "properties": {
+                        "policyDefinitionId": policy_assignment.policy_definition_id,
+                        "parameters": updated_policy_parameters
+                    }
+                }
+            )
+
+            logging.info("The policy assignment has been updated with the new allowedPrincipalIDs.")
     else:
-        logging.info("No update was needed for the policy assignment.")
-        updated_policy_assignment = policy_assignment
-
-    return func.HttpResponse(
-    json.dumps({"message": "Function completed successfully.", "updatedPolicyAssignment": updated_policy_assignment.as_dict()}),
-    status_code=200,
-    headers={"Content-Type": "application/json"}
-)
-
-
-
+        logging.error(f"Could not find the policy assignment with the ID '{assignment_id}'.")
